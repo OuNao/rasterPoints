80f717b85da52ec509c5e886ede2a75488ab476b
Sérgio Costa Fortier <sergiofortier@yahoo.com.br>
2020-04-23 14:57:39 -0300

    FIX: R CMD check fail.

--- R/rasterPoints.R
+++ R/rasterPoints.R
@@ -10,23 +10,22 @@
 #' @keywords raster, scatter, plot, points.
 #' @examples 
 #' data<-matrix(rnorm(10^6), ncol = 2)
-#' mycol<-densCols(data, colramp = rainbow)
 #' system.time({
 #'   plot(data, type="n")
-#'   points(data, pch = ".", type = "p", cex=1, col = mycol)
+#'   points(data, pch = ".", type = "p", cex=1, col = "blue")
 #' })
 #' system.time({
 #'   plot(data, type="n")
-#'   rasterPoints(data, cex=1, col = mycol)
+#'   rasterPoints(data, cex=1, col = "blue")
 #' })
 #' @export
-rasterPoints<-function(x, col, cex=1, interpolate = F, ncores = 0) {
+rasterPoints<-function(x, col="black", cex=1, interpolate = F, ncores = 0) {
   if (!is.matrix(x) || ncol(x)!=2 || nrow(x)<1) stop("x must be a matrix with 2 columns and >0 rows!", call. = F)
   if (!is.vector(col) || !is.character(col) || (length(col)!=1 && length(col) != nrow(x))) stop("col mus be a character vector of lenght 1 or nrow(x)", call. = F)
   if (!is.vector(cex) || !is.numeric(cex) || cex!=as.integer(cex) || length(cex)!=1) stop("cex must be a integer (integer vector of lenght = 1).", call. = F)
   if (!is.vector(interpolate) || !is.logical(interpolate) || length(interpolate)!=1) stop("interpolate must be a logical (TRUE or FALSE)", call. = F)
   if (!is.vector(ncores) || !is.numeric(ncores) || ncores!=as.integer(ncores) || length(ncores)!=1) stop("ncores must be a integer (integer vector of lenght = 1).", call. = F)
-  if (length(col)==1) col=do.call(rgb, as.list(col2rgb(col)/255))
+  if (length(col)==1) col=do.call(grDevices::rgb, as.list(grDevices::col2rgb(col)/255))
   usr <- graphics::par('usr')
   psize<-grDevices::dev.size('px')
   pict<-c(graphics::grconvertX(usr[1:2],"user", "ndc")*psize[1], graphics::grconvertY(usr[3:4], "user", "ndc") * psize[2])
@@ -34,7 +33,7 @@ rasterPoints<-function(x, col, cex=1, interpolate = F, ncores = 0) {
   usr<-c(graphics::grconvertX(pict[1:2]/psize[1], "ndc","user"), graphics::grconvertY(pict[3:4]/psize[2], "ndc", "user"))
   size<-c(pict[2]-pict[1]+1, pict[4]-pict[3]+1)
   img<-data2raster(x, col, usr, size[1], size[2], cex, ncores)
-  rast<-as.raster(img)
+  rast<-grDevices::as.raster(img)
   graphics::rasterImage(rast, 
                         xleft=usr[1],
                         xright=usr[2],
